67807b8ad79670c3f41e44cc98ff6b2d7b57fa30
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-16 14:42:24 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that place of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
+ * main - Entry point
+ * Return: Always 1
  */
 
 int main(void)
