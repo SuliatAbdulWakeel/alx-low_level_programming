fb7aaf32616eac28c91d68aa19734184b6176d95
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-16 09:57:34 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - A program that prints the size of various types on the computer it is compiled and run on.
- *
  * Return 0 (Success)
  */
-
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
