cbe98039a6d5674bce9bb08de9a9e27f81c5c379
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-16 07:27:18 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
+ * * Description:main - Prints out a string to stdout.
  * Return: 0 (Success)
  */
 int main(void)
