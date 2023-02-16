1df97f802b78731201414b5693d76c79767850cb
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-16 14:27:43 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints "and that place of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
