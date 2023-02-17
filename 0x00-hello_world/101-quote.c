0ae55b5cb0e2c5b69068018ea4eab192b41b727f
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-16 20:47:17 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
  * Return: Always 1
  */
 
