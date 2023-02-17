77e06d7dfc8e4c3dbd98fbab02d2c834bcbb3e6a
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-17 15:57:39 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
