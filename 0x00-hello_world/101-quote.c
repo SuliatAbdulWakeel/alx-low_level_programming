70cc613d53174daa781bb289d724b3b4d81f1ed2
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-18 00:18:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
- * Return: Always 1
+ * main - main function
+ *
+ * Return: 1 (Success)
  */
 
 int main(void)
-
 {
-	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
