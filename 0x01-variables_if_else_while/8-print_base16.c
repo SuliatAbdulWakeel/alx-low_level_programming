d28197737a1d164f1ed326a1191b170f4f52ab0a
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-17 16:08:33 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a' ; c < 'g' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
