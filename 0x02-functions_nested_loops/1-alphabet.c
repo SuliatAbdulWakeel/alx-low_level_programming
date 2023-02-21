2b066c6f9eeeb8ab32115823e526ee49bfcfa30d
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-21 06:06:04 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -  prints the alphabet, in lowercase.
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
