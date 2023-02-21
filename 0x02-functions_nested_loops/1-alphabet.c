4685125abb02f5d269498d026a7f9eee88ccf8b3
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-21 06:11:51 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,6 +14,6 @@ void print_alphabet(void)
 
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,10 +2,11 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
+
 int main(void)
+
 {
     print_alphabet();
     return (0);
