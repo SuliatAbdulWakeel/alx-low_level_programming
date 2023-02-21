ca9f4c5a190d3c47f0873af3802b551d2eb8fdb1
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-21 15:43:17 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number
+ *
  * @n: The number to be checked
  *
  * Return: 1 for positive num, -1 for negative num or zero for anything else
@@ -12,17 +13,21 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
+	_putchar('+');
+	_putchar(',');
+	_putchar(' ');
 	return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-	_putchar(45);
-	return (-1);
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	return (0);
 	}
 	else
 	{
-	_putchar(48);
-	return (0);
-	}
-}
+	_putchar('-');
+	_putchar(',');
+	_putchar(' ');
+	return (-1);
