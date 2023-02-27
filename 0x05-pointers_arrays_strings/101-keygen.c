e58811389c33bd48b8359451cb04256f4c04e3fa
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-02-27 08:53:07 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,48 +11,18 @@
 int main(void)
 
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int sum;
+	char c;
 
-	srand(time(0));
+	srand(time(NULL));
 
-	while (sum < 2772)
+	while (sum <= 2645)
 
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772)) % 2 != 0)
-		diff_half1++;
-
-		for (index = 0; password[index]; index++)
-
-		{
-			if (password[index >= (33 + diff_half1))
-
-			{
-				password[index] -= diff_half1;
-
-				break;
-			}
-		}
-		for (index = 0;  password[index]; index++)
-
-		{
-			if (password[index] >= (33 + diff_half2))
-			password[index] -= diff_half2;
-
-			break;
-		}
-	}
-}
-printf("%s", password);
-return (0);
+	putchar(2772 - sum);
+	return (0);
 }
