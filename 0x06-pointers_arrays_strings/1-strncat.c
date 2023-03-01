e67c7e143c44c2142f7372f4c671ce3aea88ec25
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-01 09:02:33 -0800

    Task 0-4

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: The string to be appended upon
+ * @src: The string to be appended to dest
+ * @n: The number of bytes
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+	dest[dest_len++] = src[index];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * @str: The string to be changed
+ * Return: A pointer to the changed string
+ */
+
+char *string_toupper(char *)
+
+{
+	int index = 0;
+
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+}
