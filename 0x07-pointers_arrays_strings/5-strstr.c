a95b5f91a113890174371953872084a08ad06659
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-06 06:49:51 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
