3848e78fcfbf9a19c061add8ef621049c28fba4a
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-06 07:16:07 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, n;
+	unsigned int i, m = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 64; i++)
 	{
-		for (n = 0; n < 8; n++)
-			_putchar(a[i][n]);
-		_putchar('\n');
+		if (i % 8 == 0 && i != 0)
+		{
+			m = 1;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
 	}
+		_putchar('\n');
 }
