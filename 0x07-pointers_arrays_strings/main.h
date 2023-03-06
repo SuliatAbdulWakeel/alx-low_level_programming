a33c51803c59bec7b93f928102c9a5432b228de2
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-06 09:32:03 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c)
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
