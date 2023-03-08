0669f3c6ff04cc1005fbed440fe6785daff9866a
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-08 00:02:15 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,18 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: The atring
+ * @s: The string
  * Return: 0
  */
 
 void _puts_recursion(char *s)
-
 {
 	if (*s)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
 
 	else
-		_putchar('\n');
+	_putchar('\n');
 }
