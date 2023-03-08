849f85d949789c30df2c6ba77184cbbf91acd6d4
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-07 23:51:02 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: The atring
+ * Return: 0
  */
 
 void _puts_recursion(char *s)
@@ -11,7 +12,6 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-
 		_puts_recursion(s + 1);
 	}
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
-
-int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
