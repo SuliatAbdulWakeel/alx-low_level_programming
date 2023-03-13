52a671d3a2685e47903e51ee978c056812e971c5
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-13 06:07:44 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *str;
+
 	unsigned int i;
-	char *s;
 
-	if (size == 0)
-		return (NULL);
+	str = malloc(sizeof(char) * size);
 
-	S = malloc(size * sizeof(char));
+	if (size == 0 || str == NULL)
 
-	if (s == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-	{
-		s[i] = c;
-	}
 
-	return (s);
+		str[i] = c;
+
+	return (str);
 }
