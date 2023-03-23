78ff91ff4b833fe51ee75266d0a381e6dafb334c
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-23 14:22:50 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,22 +12,27 @@
  * If one of the strings if NULL, (nil) is printed instead.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
+
 {
 	va_list strings;
 	char *str;
 	unsigned int index;
 
 	va_start(strings, n);
+
 	for (index = 0; index < n; index++)
-		{
-			str = va_arg(strings, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			if (index != (n - 1) && separator != NULL)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(strings);
+	{
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(strings);
 }
